23052c09a4a21e5c3aadc0e06c43f1453cf2c496
emrullahyildirim <emrullahyildirim08@gmail.com>
2026-01-01 22:25:55 +0300

    Added secrets to handle passwords. Moved important stuff to env file.

--- srcs/mariadb/scripts/docker-entrypoint.sh
+++ srcs/mariadb/scripts/docker-entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+set -e
+
+MYSQL_ROOT_PASSWORD_FILE="/run/secrets/db_root_password"
+DB_USER_PASSWORD_FILE="/run/secrets/db_user_password"
+
+if [ -f "$MYSQL_ROOT_PASSWORD_FILE" ]; then
+	MYSQL_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+else
+	echo "Database root password file not found!"
+	exit 1
+fi
+
+if [ -f "$DB_USER_PASSWORD_FILE" ]; then
+	DB_PASSWORD=$(cat "$DB_USER_PASSWORD_FILE")
+else
+	echo "Database user password file not found!"
+	exit 1
+fi
+
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
@@ -12,5 +31,9 @@ CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;" > /tmp/init.sql
+
+
+unset MYSQL_ROOT_PASSWORD
+unset DB_PASSWORD
 
 exec "$@"
--- srcs/wordpress/scripts/docker-entrypoint.sh
+++ srcs/wordpress/scripts/docker-entrypoint.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 
+set -e
+
 WP_PATH="${WP_PATH:-/var/www/html}"
+
+ADMIN_PASSWORD_FILE="/run/secrets/wp_admin_password"
+SITE_USER_PASSWORD_FILE="/run/secrets/wp_user_password"
+DB_PASSWORD_FILE="/run/secrets/db_user_password"
+
+if [ -f "$ADMIN_PASSWORD_FILE" ]; then
+	ADMIN_PASSWORD=$(cat "$ADMIN_PASSWORD_FILE")
+else
+	echo "Admin password file not found!"
+	exit 1
+fi
+
+if [ -f "$SITE_USER_PASSWORD_FILE" ]; then
+	SITE_USER_PASSWORD=$(cat "$SITE_USER_PASSWORD_FILE")
+else
+	echo "Site user password file not found!"
+	exit 1
+fi
+
+if [ -f "$DB_PASSWORD_FILE" ]; then
+	DB_PASSWORD=$(cat "$DB_PASSWORD_FILE")
+else
+	echo "Database user password file not found!"
+	exit 1
+fi
 
 if [ ! -f "$WP_PATH/wp-settings.php" ]; then
     wp core download --path="$WP_PATH" --allow-root
@@ -32,5 +59,10 @@ if [ ! -f "$WP_PATH/wp-config.php" ]; then
 		--path="$WP_PATH" \
 		--allow-root
 fi
+
+
+unset ADMIN_PASSWORD
+unset SITE_USER_PASSWORD
+unset DB_PASSWORD
 
 exec "$@"
